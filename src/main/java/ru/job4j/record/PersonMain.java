3e877145d8140a6e0e25659d65d59eba999a71c8
1oogabooga1 <dmitrijdobrydin@gmail.com>
2023-10-24 17:38:15 +0300

    Record

---
+++ src/main/java/ru/job4j/record/PersonMain.java
@@ -0,0 +1,8 @@
+package ru.job4j.record;
+
+public class PersonMain {
+    public static void main(String[] args) {
+        PersonRecord record = new PersonRecord("Иван", 30);
+        System.out.println(record);
+    }
+}
