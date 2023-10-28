d3cee988a6d0b97535abc2de29e132bfe5399184
1oogabooga1 <dmitrijdobrydin@gmail.com>
2023-10-28 22:57:44 +0300

    fix

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -67,6 +67,7 @@ class StartUITest {
         assertThat(out.toString()).isEqualTo(
                 "Меню:" + System.lineSeparator()
                         + "0. Завершить программу" + System.lineSeparator()
+                + "=== Завершение программы ===" + System.lineSeparator()
         );
     }
 
