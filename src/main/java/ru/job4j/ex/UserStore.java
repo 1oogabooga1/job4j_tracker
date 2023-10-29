2694cda81b931d9e85650ddf8c7dc602038d4adf
1oogabooga1 <dmitrijdobrydin@gmail.com>
2023-10-29 16:19:03 +0300

    0.3. Тестирование исключений с junit [#219366 #456252]

---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,40 @@
+package ru.job4j.ex;
+
+public class UserStore {
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        User user = null;
+        for (User value : users) {
+            if (login.equals(value.getUsername())) {
+                user = value;
+                break;
+            }
+        }
+        if (user == null) {
+            throw new UserNotFoundException("User not found");
+        }
+        return user;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (!user.isValid() || user.getUsername().length() < 3) {
+            throw new UserInvalidException("User is invalid");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+        try {
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException ea) {
+            ea.printStackTrace();
+        } catch (UserNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
