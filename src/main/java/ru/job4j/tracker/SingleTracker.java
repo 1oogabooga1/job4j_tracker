0af0abfb6451ae0b9ba779a0bc12a9f7736163b3
1oogabooga1 <dmitrijdobrydin@gmail.com>
2023-10-30 14:04:32 +0300

    1. Singleton [#94606 #456563]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -3,6 +3,15 @@ package ru.job4j.tracker;
 public class SingleTracker {
     private Tracker tracker = new Tracker();
 
+    private static SingleTracker instance = null;
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
+
     public Item add(Item item) {
         return tracker.add(item);
     }
