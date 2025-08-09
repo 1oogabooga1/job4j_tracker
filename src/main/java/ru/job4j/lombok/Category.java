26b1293873dedcf538ac157bc439b5676454bf16
Dmitrii <dmitrijdobrydin@gmail.com>
2025-08-09 13:10:45 +0300

    2. @Setter, @Getter и другие [#504889]

---
+++ src/main/java/ru/job4j/lombok/Category.java
@@ -0,0 +1,17 @@
+package ru.job4j.lombok;
+
+import lombok.*;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@RequiredArgsConstructor
+public class Category {
+
+    @EqualsAndHashCode.Include
+    @Getter
+    @NonNull
+    private final int id;
+
+    @Getter
+    @Setter
+    private String name;
+}
