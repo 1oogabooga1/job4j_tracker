6cc87108c801c981949b6a378e5cd47746c37c8e
Dmitrii <dmitrijdobrydin@gmail.com>
2025-11-04 12:58:26 +0300

    3. @Builder [#504890 #550227]

---
+++ src/main/java/ru/job4j/lombok/Permission.java
@@ -0,0 +1,19 @@
+package ru.job4j.lombok;
+
+import lombok.*;
+
+import java.util.List;
+
+@Builder(builderMethodName = "of")
+@ToString
+@Getter
+public class Permission {
+    private Integer id;
+
+    private String name;
+
+    @Singular("rules")
+    private List<String> rules;
+}
+
+
